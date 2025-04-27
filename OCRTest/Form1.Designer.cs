33c7f3eb19dd5292d5045be11299d7d80327528c
hamletzhang <HamletZhang@qq.com>
2025-04-27 15:49:33 +0800

    完成了UI 无法运行

--- OCRTest/Form1.Designer.cs
+++ OCRTest/Form1.Designer.cs
@@ -28,13 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnLoadDetectModel = new System.Windows.Forms.Button();
+            this.btnLoadRecognizeModel = new System.Windows.Forms.Button();
+            this.btnOpenImage = new System.Windows.Forms.Button();
+            this.btnInfer = new System.Windows.Forms.Button();
+            this.btnFreeModel = new System.Windows.Forms.Button();
+            this.comboBoxDevices = new System.Windows.Forms.ComboBox();
+            this.labelDevice = new System.Windows.Forms.Label();
+            this.imageViewer = new DLCV.ImageViewer();
+            this.richTextBoxResult = new System.Windows.Forms.RichTextBox();
+            this.labelDetectModel = new System.Windows.Forms.Label();
+            this.labelRecognizeModel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLoadDetectModel
+            // 
+            this.btnLoadDetectModel.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnLoadDetectModel.Location = new System.Drawing.Point(14, 19);
+            this.btnLoadDetectModel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLoadDetectModel.Name = "btnLoadDetectModel";
+            this.btnLoadDetectModel.Size = new System.Drawing.Size(140, 40);
+            this.btnLoadDetectModel.TabIndex = 1;
+            this.btnLoadDetectModel.Text = "加载检测模型";
+            this.btnLoadDetectModel.UseVisualStyleBackColor = true;
+            this.btnLoadDetectModel.Click += new System.EventHandler(this.btnLoadDetectModel_Click);
+            // 
+            // btnLoadRecognizeModel
+            // 
+            this.btnLoadRecognizeModel.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnLoadRecognizeModel.Location = new System.Drawing.Point(14, 70);
+            this.btnLoadRecognizeModel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLoadRecognizeModel.Name = "btnLoadRecognizeModel";
+            this.btnLoadRecognizeModel.Size = new System.Drawing.Size(140, 40);
+            this.btnLoadRecognizeModel.TabIndex = 2;
+            this.btnLoadRecognizeModel.Text = "加载识别模型";
+            this.btnLoadRecognizeModel.UseVisualStyleBackColor = true;
+            this.btnLoadRecognizeModel.Click += new System.EventHandler(this.btnLoadRecognizeModel_Click);
+            // 
+            // btnOpenImage
+            // 
+            this.btnOpenImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpenImage.Location = new System.Drawing.Point(648, 19);
+            this.btnOpenImage.Name = "btnOpenImage";
+            this.btnOpenImage.Size = new System.Drawing.Size(140, 40);
+            this.btnOpenImage.TabIndex = 3;
+            this.btnOpenImage.Text = "打开图片";
+            this.btnOpenImage.UseVisualStyleBackColor = true;
+            this.btnOpenImage.Click += new System.EventHandler(this.btnOpenImage_Click);
+            // 
+            // btnInfer
+            // 
+            this.btnInfer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInfer.Location = new System.Drawing.Point(648, 70);
+            this.btnInfer.Name = "btnInfer";
+            this.btnInfer.Size = new System.Drawing.Size(140, 40);
+            this.btnInfer.TabIndex = 4;
+            this.btnInfer.Text = "OCR推理";
+            this.btnInfer.UseVisualStyleBackColor = true;
+            this.btnInfer.Click += new System.EventHandler(this.btnInfer_Click);
+            // 
+            // btnFreeModel
+            // 
+            this.btnFreeModel.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnFreeModel.Location = new System.Drawing.Point(14, 121);
+            this.btnFreeModel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFreeModel.Name = "btnFreeModel";
+            this.btnFreeModel.Size = new System.Drawing.Size(140, 40);
+            this.btnFreeModel.TabIndex = 8;
+            this.btnFreeModel.Text = "释放模型";
+            this.btnFreeModel.UseVisualStyleBackColor = true;
+            this.btnFreeModel.Click += new System.EventHandler(this.btnFreeModel_Click);
+            // 
+            // comboBoxDevices
+            // 
+            this.comboBoxDevices.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxDevices.FormattingEnabled = true;
+            this.comboBoxDevices.Location = new System.Drawing.Point(249, 24);
+            this.comboBoxDevices.Name = "comboBoxDevices";
+            this.comboBoxDevices.Size = new System.Drawing.Size(393, 32);
+            this.comboBoxDevices.TabIndex = 12;
+            // 
+            // labelDevice
+            // 
+            this.labelDevice.AutoSize = true;
+            this.labelDevice.Location = new System.Drawing.Point(175, 27);
+            this.labelDevice.Name = "labelDevice";
+            this.labelDevice.Size = new System.Drawing.Size(68, 24);
+            this.labelDevice.TabIndex = 13;
+            this.labelDevice.Text = "GPU：";
+            // 
+            // imageViewer
+            // 
+            this.imageViewer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.imageViewer.Location = new System.Drawing.Point(14, 169);
+            this.imageViewer.MaxScale = 100F;
+            this.imageViewer.MinScale = 0.5F;
+            this.imageViewer.Name = "imageViewer";
+            this.imageViewer.ShowStatusText = false;
+            this.imageViewer.Size = new System.Drawing.Size(513, 269);
+            this.imageViewer.TabIndex = 15;
+            // 
+            // richTextBoxResult
+            // 
+            this.richTextBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxResult.Location = new System.Drawing.Point(533, 169);
+            this.richTextBoxResult.Name = "richTextBoxResult";
+            this.richTextBoxResult.Size = new System.Drawing.Size(255, 269);
+            this.richTextBoxResult.TabIndex = 16;
+            this.richTextBoxResult.Text = "";
+            // 
+            // labelDetectModel
+            // 
+            this.labelDetectModel.AutoSize = true;
+            this.labelDetectModel.Location = new System.Drawing.Point(175, 78);
+            this.labelDetectModel.Name = "labelDetectModel";
+            this.labelDetectModel.Size = new System.Drawing.Size(154, 24);
+            this.labelDetectModel.TabIndex = 18;
+            this.labelDetectModel.Text = "检测模型：未加载";
+            // 
+            // labelRecognizeModel
+            // 
+            this.labelRecognizeModel.AutoSize = true;
+            this.labelRecognizeModel.Location = new System.Drawing.Point(350, 78);
+            this.labelRecognizeModel.Name = "labelRecognizeModel";
+            this.labelRecognizeModel.Size = new System.Drawing.Size(154, 24);
+            this.labelRecognizeModel.TabIndex = 19;
+            this.labelRecognizeModel.Text = "识别模型：未加载";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.labelRecognizeModel);
+            this.Controls.Add(this.labelDetectModel);
+            this.Controls.Add(this.richTextBoxResult);
+            this.Controls.Add(this.imageViewer);
+            this.Controls.Add(this.labelDevice);
+            this.Controls.Add(this.comboBoxDevices);
+            this.Controls.Add(this.btnFreeModel);
+            this.Controls.Add(this.btnInfer);
+            this.Controls.Add(this.btnOpenImage);
+            this.Controls.Add(this.btnLoadRecognizeModel);
+            this.Controls.Add(this.btnLoadDetectModel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "OCR测试程序";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLoadDetectModel;
+        private System.Windows.Forms.Button btnLoadRecognizeModel;
+        private System.Windows.Forms.Button btnOpenImage;
+        private System.Windows.Forms.Button btnInfer;
+        private System.Windows.Forms.Button btnFreeModel;
+        private System.Windows.Forms.ComboBox comboBoxDevices;
+        private System.Windows.Forms.Label labelDevice;
+        private DLCV.ImageViewer imageViewer;
+        private System.Windows.Forms.RichTextBox richTextBoxResult;
+        private System.Windows.Forms.Label labelDetectModel;
+        private System.Windows.Forms.Label labelRecognizeModel;
     }
 }
 
--- OCRTest/Form1.cs
+++ OCRTest/Form1.cs
@@ -3,18 +3,293 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using dlcv_infer_csharp;
+using Newtonsoft.Json.Linq;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using static dlcv_infer_csharp.Utils;
 
 namespace OCRTest
 {
     public partial class Form1 : Form
     {
+        private Model _detectModel; // 检测模型
+        private Model _recognizeModel; // 识别模型
+        private string _imagePath; // 图像路径
+        private bool _isDetectModelLoaded = false; // 检测模型是否加载
+        private bool _isRecognizeModelLoaded = false; // 识别模型是否加载
+        private int _deviceId = 0; // GPU设备ID
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 初始化窗体和获取GPU设备信息
+            TopMost = false;
+            Thread thread = new Thread(GetDeviceInfo);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        /// <summary>
+        /// 获取GPU设备信息并填充到下拉框
+        /// </summary>
+        private void GetDeviceInfo()
+        {
+            Thread.Sleep(1);
+            JObject device_info = Utils.GetGpuInfo();
+
+            Invoke((MethodInvoker)delegate
+            {
+                if (device_info.GetValue("code").ToString() == "0")
+                {
+                    foreach (var item in device_info.GetValue("devices"))
+                    {
+                        comboBoxDevices.Items.Add(item["device_name"].ToString());
+                    }
+                }
+                else
+                {
+                    richTextBoxResult.Text = device_info.ToString();
+                }
+
+                if (comboBoxDevices.Items.Count == 0)
+                {
+                    comboBoxDevices.Items.Add("Unknown");
+                }
+                comboBoxDevices.SelectedIndex = 0;
+            });
+        }
+
+        /// <summary>
+        /// 加载检测模型
+        /// </summary>
+        private void btnLoadDetectModel_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.RestoreDirectory = true;
+
+            openFileDialog.Filter = "深度视觉加速模型文件 (*.dvt)|*.dvt";
+            openFileDialog.Title = "选择检测模型";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                _deviceId = comboBoxDevices.SelectedIndex;
+                try
+                {
+                    // 释放之前的模型资源（如果有）
+                    if (_isDetectModelLoaded)
+                    {
+                        _detectModel = null;
+                        GC.Collect();
+                    }
+
+                    // 加载新模型
+                    _detectModel = new Model(selectedFilePath, _deviceId);
+                    _isDetectModelLoaded = true;
+                    labelDetectModel.Text = $"检测模型：{Path.GetFileName(selectedFilePath)}";
+                    richTextBoxResult.Text = "检测模型加载成功！";
+                }
+                catch (Exception ex)
+                {
+                    richTextBoxResult.Text = $"检测模型加载错误：{ex.Message}";
+                }
+            }
+        }
+
+        /// <summary>
+        /// 加载识别模型
+        /// </summary>
+        private void btnLoadRecognizeModel_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.RestoreDirectory = true;
+
+            openFileDialog.Filter = "深度视觉加速模型文件 (*.dvt)|*.dvt";
+            openFileDialog.Title = "选择识别模型";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                _deviceId = comboBoxDevices.SelectedIndex;
+                try
+                {
+                    // 释放之前的模型资源（如果有）
+                    if (_isRecognizeModelLoaded)
+                    {
+                        _recognizeModel = null;
+                        GC.Collect();
+                    }
+
+                    // 加载新模型
+                    _recognizeModel = new Model(selectedFilePath, _deviceId);
+                    _isRecognizeModelLoaded = true;
+                    labelRecognizeModel.Text = $"识别模型：{Path.GetFileName(selectedFilePath)}";
+                    richTextBoxResult.Text = "识别模型加载成功！";
+                }
+                catch (Exception ex)
+                {
+                    richTextBoxResult.Text = $"识别模型加载错误：{ex.Message}";
+                }
+            }
+        }
+
+        /// <summary>
+        /// 打开图片
+        /// </summary>
+        private void btnOpenImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "图片文件 (*.jpg;*.jpeg;*.png;*.bmp;*.gif)|*.jpg;*.jpeg;*.png;*.bmp;*.gif|所有文件 (*.*)|*.*";
+            openFileDialog.Title = "选择图片文件";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _imagePath = openFileDialog.FileName;
+                try
+                {
+                    // 加载并显示图像
+                    using (Bitmap bitmap = new Bitmap(_imagePath))
+                    {
+                        imageViewer.UpdateImage(bitmap);
+                        richTextBoxResult.Text = $"已加载图片：{Path.GetFileName(_imagePath)}";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    richTextBoxResult.Text = $"加载图片失败：{ex.Message}";
+                }
+            }
+        }
+
+        /// <summary>
+        /// 执行OCR推理
+        /// </summary>
+        private void btnInfer_Click(object sender, EventArgs e)
+        {
+            if (!_isDetectModelLoaded || !_isRecognizeModelLoaded)
+            {
+                MessageBox.Show("请先加载检测模型和识别模型！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(_imagePath))
+            {
+                MessageBox.Show("请先选择图片文件！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                // 加载图像
+                Mat image = Cv2.ImRead(_imagePath, ImreadModes.Color);
+                Mat imageRgb = new Mat();
+                Cv2.CvtColor(image, imageRgb, ColorConversionCodes.BGR2RGB);
+
+                if (image.Empty())
+                {
+                    richTextBoxResult.Text = "图像解码失败！";
+                    return;
+                }
+
+                // 执行OCR推理
+                richTextBoxResult.Text = "开始执行OCR推理...";
+                CSharpResult result = Utils.OcrInfer(_detectModel, _recognizeModel, imageRgb);
+
+                // 更新图像显示
+                using (Bitmap bitmap = BitmapConverter.ToBitmap(image))
+                {
+                    imageViewer.UpdateImageAndResult(bitmap, result);
+                }
+
+                // 显示结果
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("OCR推理结果：");
+                sb.AppendLine("----------------------");
+
+                if (result.SampleResults.Count > 0)
+                {
+                    var sampleResult = result.SampleResults[0];
+                    foreach (var item in sampleResult.Results)
+                    {
+                        sb.AppendLine($"文本：{item.CategoryName}");
+                        sb.AppendLine($"置信度：{item.Score:F2}");
+                        sb.AppendLine($"位置：[{item.Bbox[0]:F0},{item.Bbox[1]:F0},{item.Bbox[2]:F0},{item.Bbox[3]:F0}]");
+                        sb.AppendLine("----------------------");
+                    }
+                }
+                else
+                {
+                    sb.AppendLine("未检测到文本");
+                }
+
+                richTextBoxResult.Text = sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                richTextBoxResult.Text = $"OCR推理失败：{ex.Message}";
+            }
+        }
+
+        /// <summary>
+        /// 释放模型资源
+        /// </summary>
+        private void btnFreeModel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_isDetectModelLoaded)
+                {
+                    _detectModel = null;
+                    _isDetectModelLoaded = false;
+                    labelDetectModel.Text = "检测模型：未加载";
+                }
+
+                if (_isRecognizeModelLoaded)
+                {
+                    _recognizeModel = null;
+                    _isRecognizeModelLoaded = false;
+                    labelRecognizeModel.Text = "识别模型：未加载";
+                }
+
+                GC.Collect();
+                richTextBoxResult.Text = "模型已释放";
+            }
+            catch (Exception ex)
+            {
+                richTextBoxResult.Text = $"释放模型失败：{ex.Message}";
+            }
+        }
+
+        /// <summary>
+        /// 窗体关闭时释放资源
+        /// </summary>
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // 释放模型资源
+            if (_isDetectModelLoaded)
+            {
+                _detectModel = null;
+                _isDetectModelLoaded = false;
+            }
+
+            if (_isRecognizeModelLoaded)
+            {
+                _recognizeModel = null;
+                _isRecognizeModelLoaded = false;
+            }
+
+            GC.Collect();
         }
     }
 }
