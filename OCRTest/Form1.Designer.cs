060de14c3b4c2bfff56bdba9c52eff618a001e99
Lu Qi <55431358+pudgyLu@users.noreply.github.com>
2025-04-30 13:25:02 +0800

    新增压力测试功能
    * 新增目标检测+OCR压测功能
    * 调整加载图片功能
    图片加载 -> 图片加载并推理
    * 调整了每个按键的位置

--- OCRTest/Form1.Designer.cs
+++ OCRTest/Form1.Designer.cs
@@ -31,7 +31,6 @@
             this.btnLoadDetectModel = new System.Windows.Forms.Button();
             this.btnLoadRecognizeModel = new System.Windows.Forms.Button();
             this.btnOpenImage = new System.Windows.Forms.Button();
-            this.btnInfer = new System.Windows.Forms.Button();
             this.btnFreeModel = new System.Windows.Forms.Button();
             this.comboBoxDevices = new System.Windows.Forms.ComboBox();
             this.labelDevice = new System.Windows.Forms.Label();
@@ -39,11 +38,12 @@
             this.richTextBoxResult = new System.Windows.Forms.RichTextBox();
             this.labelDetectModel = new System.Windows.Forms.Label();
             this.labelRecognizeModel = new System.Windows.Forms.Label();
+            this.btnStartStressTest = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnLoadDetectModel
             // 
-            this.btnLoadDetectModel.Location = new System.Drawing.Point(11, 14);
+            this.btnLoadDetectModel.Location = new System.Drawing.Point(21, 12);
             this.btnLoadDetectModel.Name = "btnLoadDetectModel";
             this.btnLoadDetectModel.Size = new System.Drawing.Size(127, 32);
             this.btnLoadDetectModel.TabIndex = 1;
@@ -53,7 +53,7 @@
             // 
             // btnLoadRecognizeModel
             // 
-            this.btnLoadRecognizeModel.Location = new System.Drawing.Point(11, 52);
+            this.btnLoadRecognizeModel.Location = new System.Drawing.Point(21, 52);
             this.btnLoadRecognizeModel.Name = "btnLoadRecognizeModel";
             this.btnLoadRecognizeModel.Size = new System.Drawing.Size(127, 33);
             this.btnLoadRecognizeModel.TabIndex = 2;
@@ -64,30 +64,18 @@
             // btnOpenImage
             // 
             this.btnOpenImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnOpenImage.Location = new System.Drawing.Point(977, 14);
-            this.btnOpenImage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOpenImage.Location = new System.Drawing.Point(21, 93);
+            this.btnOpenImage.Margin = new System.Windows.Forms.Padding(2);
             this.btnOpenImage.Name = "btnOpenImage";
-            this.btnOpenImage.Size = new System.Drawing.Size(118, 32);
+            this.btnOpenImage.Size = new System.Drawing.Size(180, 32);
             this.btnOpenImage.TabIndex = 3;
-            this.btnOpenImage.Text = "打开图片";
+            this.btnOpenImage.Text = "打开图片并推理";
             this.btnOpenImage.UseVisualStyleBackColor = true;
             this.btnOpenImage.Click += new System.EventHandler(this.btnOpenImage_Click);
             // 
-            // btnInfer
-            // 
-            this.btnInfer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnInfer.Location = new System.Drawing.Point(977, 52);
-            this.btnInfer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btnInfer.Name = "btnInfer";
-            this.btnInfer.Size = new System.Drawing.Size(118, 32);
-            this.btnInfer.TabIndex = 4;
-            this.btnInfer.Text = "OCR推理";
-            this.btnInfer.UseVisualStyleBackColor = true;
-            this.btnInfer.Click += new System.EventHandler(this.btnInfer_Click);
-            // 
             // btnFreeModel
             // 
-            this.btnFreeModel.Location = new System.Drawing.Point(11, 91);
+            this.btnFreeModel.Location = new System.Drawing.Point(1101, 28);
             this.btnFreeModel.Name = "btnFreeModel";
             this.btnFreeModel.Size = new System.Drawing.Size(127, 30);
             this.btnFreeModel.TabIndex = 8;
@@ -100,16 +88,16 @@
             this.comboBoxDevices.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.comboBoxDevices.FormattingEnabled = true;
-            this.comboBoxDevices.Location = new System.Drawing.Point(204, 18);
-            this.comboBoxDevices.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBoxDevices.Location = new System.Drawing.Point(505, 137);
+            this.comboBoxDevices.Margin = new System.Windows.Forms.Padding(2);
             this.comboBoxDevices.Name = "comboBoxDevices";
-            this.comboBoxDevices.Size = new System.Drawing.Size(590, 26);
+            this.comboBoxDevices.Size = new System.Drawing.Size(723, 26);
             this.comboBoxDevices.TabIndex = 12;
             // 
             // labelDevice
             // 
             this.labelDevice.AutoSize = true;
-            this.labelDevice.Location = new System.Drawing.Point(143, 20);
+            this.labelDevice.Location = new System.Drawing.Point(435, 140);
             this.labelDevice.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelDevice.Name = "labelDevice";
             this.labelDevice.Size = new System.Drawing.Size(53, 18);
@@ -123,30 +111,30 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.imageViewer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.imageViewer.image = null;
-            this.imageViewer.Location = new System.Drawing.Point(274, 126);
-            this.imageViewer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.imageViewer.Location = new System.Drawing.Point(274, 183);
+            this.imageViewer.Margin = new System.Windows.Forms.Padding(2);
             this.imageViewer.MaxScale = 100F;
             this.imageViewer.MinScale = 0.5F;
             this.imageViewer.Name = "imageViewer";
             this.imageViewer.ShowStatusText = false;
-            this.imageViewer.Size = new System.Drawing.Size(821, 940);
+            this.imageViewer.Size = new System.Drawing.Size(954, 883);
             this.imageViewer.TabIndex = 15;
             // 
             // richTextBoxResult
             // 
             this.richTextBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.richTextBoxResult.Location = new System.Drawing.Point(11, 126);
-            this.richTextBoxResult.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBoxResult.Location = new System.Drawing.Point(19, 182);
+            this.richTextBoxResult.Margin = new System.Windows.Forms.Padding(2);
             this.richTextBoxResult.Name = "richTextBoxResult";
-            this.richTextBoxResult.Size = new System.Drawing.Size(248, 941);
+            this.richTextBoxResult.Size = new System.Drawing.Size(248, 884);
             this.richTextBoxResult.TabIndex = 16;
             this.richTextBoxResult.Text = "";
             // 
             // labelDetectModel
             // 
             this.labelDetectModel.AutoSize = true;
-            this.labelDetectModel.Location = new System.Drawing.Point(143, 58);
+            this.labelDetectModel.Location = new System.Drawing.Point(153, 19);
             this.labelDetectModel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelDetectModel.Name = "labelDetectModel";
             this.labelDetectModel.Size = new System.Drawing.Size(152, 18);
@@ -156,18 +144,28 @@
             // labelRecognizeModel
             // 
             this.labelRecognizeModel.AutoSize = true;
-            this.labelRecognizeModel.Location = new System.Drawing.Point(626, 58);
+            this.labelRecognizeModel.Location = new System.Drawing.Point(162, 59);
             this.labelRecognizeModel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelRecognizeModel.Name = "labelRecognizeModel";
             this.labelRecognizeModel.Size = new System.Drawing.Size(143, 18);
             this.labelRecognizeModel.TabIndex = 19;
             this.labelRecognizeModel.Text = "OCR模型：未加载";
             // 
+            // btnStartStressTest
+            // 
+            this.btnStartStressTest.Location = new System.Drawing.Point(21, 137);
+            this.btnStartStressTest.Name = "btnStartStressTest";
+            this.btnStartStressTest.Size = new System.Drawing.Size(180, 40);
+            this.btnStartStressTest.TabIndex = 5;
+            this.btnStartStressTest.Text = "开始压力测试";
+            this.btnStartStressTest.UseVisualStyleBackColor = true;
+            this.btnStartStressTest.Click += new System.EventHandler(this.btnStartStressTest_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1106, 1077);
+            this.ClientSize = new System.Drawing.Size(1239, 1077);
             this.Controls.Add(this.richTextBoxResult);
             this.Controls.Add(this.labelRecognizeModel);
             this.Controls.Add(this.labelDetectModel);
@@ -175,11 +173,11 @@
             this.Controls.Add(this.labelDevice);
             this.Controls.Add(this.comboBoxDevices);
             this.Controls.Add(this.btnFreeModel);
-            this.Controls.Add(this.btnInfer);
             this.Controls.Add(this.btnOpenImage);
             this.Controls.Add(this.btnLoadRecognizeModel);
             this.Controls.Add(this.btnLoadDetectModel);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Controls.Add(this.btnStartStressTest);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "OCR测试程序";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
@@ -194,7 +192,6 @@
         private System.Windows.Forms.Button btnLoadDetectModel;
         private System.Windows.Forms.Button btnLoadRecognizeModel;
         private System.Windows.Forms.Button btnOpenImage;
-        private System.Windows.Forms.Button btnInfer;
         private System.Windows.Forms.Button btnFreeModel;
         private System.Windows.Forms.ComboBox comboBoxDevices;
         private System.Windows.Forms.Label labelDevice;
@@ -202,6 +199,7 @@
         private System.Windows.Forms.RichTextBox richTextBoxResult;
         private System.Windows.Forms.Label labelDetectModel;
         private System.Windows.Forms.Label labelRecognizeModel;
+        private System.Windows.Forms.Button btnStartStressTest;
     }
 }
 
