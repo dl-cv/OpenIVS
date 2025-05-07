d59bd60af8841fd24ca3b3265bdd7ff12c462f88
杨培文 (Yang Peiwen) <915505626@qq.com>
2025-05-07 16:49:22 +0800

    ui

--- DlcvDemo/SlidingWindowConfigForm.Designer.cs
+++ DlcvDemo/SlidingWindowConfigForm.Designer.cs
@@ -49,121 +49,152 @@ namespace DlcvDemo
             // lblWidth
             // 
             this.lblWidth.AutoSize = true;
-            this.lblWidth.Location = new System.Drawing.Point(20, 20);
+            this.lblWidth.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblWidth.Location = new System.Drawing.Point(30, 30);
+            this.lblWidth.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblWidth.Name = "lblWidth";
-            this.lblWidth.Size = new System.Drawing.Size(65, 12);
+            this.lblWidth.Size = new System.Drawing.Size(104, 24);
             this.lblWidth.TabIndex = 0;
             this.lblWidth.Text = "小图像宽度:";
             // 
             // txtWidth
             // 
-            this.txtWidth.Location = new System.Drawing.Point(150, 20);
+            this.txtWidth.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtWidth.Location = new System.Drawing.Point(154, 26);
+            this.txtWidth.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtWidth.Name = "txtWidth";
-            this.txtWidth.Size = new System.Drawing.Size(200, 21);
+            this.txtWidth.Size = new System.Drawing.Size(298, 31);
             this.txtWidth.TabIndex = 1;
             // 
             // lblHeight
             // 
             this.lblHeight.AutoSize = true;
-            this.lblHeight.Location = new System.Drawing.Point(20, 60);
+            this.lblHeight.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblHeight.Location = new System.Drawing.Point(30, 90);
+            this.lblHeight.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblHeight.Name = "lblHeight";
-            this.lblHeight.Size = new System.Drawing.Size(65, 12);
+            this.lblHeight.Size = new System.Drawing.Size(104, 24);
             this.lblHeight.TabIndex = 2;
             this.lblHeight.Text = "小图像高度:";
             // 
             // txtHeight
             // 
-            this.txtHeight.Location = new System.Drawing.Point(150, 60);
+            this.txtHeight.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtHeight.Location = new System.Drawing.Point(154, 86);
+            this.txtHeight.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtHeight.Name = "txtHeight";
-            this.txtHeight.Size = new System.Drawing.Size(200, 21);
+            this.txtHeight.Size = new System.Drawing.Size(298, 31);
             this.txtHeight.TabIndex = 3;
             // 
             // lblHOverlap
             // 
             this.lblHOverlap.AutoSize = true;
-            this.lblHOverlap.Location = new System.Drawing.Point(20, 100);
+            this.lblHOverlap.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblHOverlap.Location = new System.Drawing.Point(30, 150);
+            this.lblHOverlap.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblHOverlap.Name = "lblHOverlap";
-            this.lblHOverlap.Size = new System.Drawing.Size(53, 12);
+            this.lblHOverlap.Size = new System.Drawing.Size(86, 24);
             this.lblHOverlap.TabIndex = 4;
             this.lblHOverlap.Text = "水平重叠:";
             // 
             // txtHOverlap
             // 
-            this.txtHOverlap.Location = new System.Drawing.Point(150, 100);
+            this.txtHOverlap.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtHOverlap.Location = new System.Drawing.Point(154, 146);
+            this.txtHOverlap.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtHOverlap.Name = "txtHOverlap";
-            this.txtHOverlap.Size = new System.Drawing.Size(200, 21);
+            this.txtHOverlap.Size = new System.Drawing.Size(298, 31);
             this.txtHOverlap.TabIndex = 5;
             // 
             // lblVOverlap
             // 
             this.lblVOverlap.AutoSize = true;
-            this.lblVOverlap.Location = new System.Drawing.Point(20, 140);
+            this.lblVOverlap.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblVOverlap.Location = new System.Drawing.Point(30, 210);
+            this.lblVOverlap.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblVOverlap.Name = "lblVOverlap";
-            this.lblVOverlap.Size = new System.Drawing.Size(53, 12);
+            this.lblVOverlap.Size = new System.Drawing.Size(86, 24);
             this.lblVOverlap.TabIndex = 6;
             this.lblVOverlap.Text = "垂直重叠:";
             // 
             // txtVOverlap
             // 
-            this.txtVOverlap.Location = new System.Drawing.Point(150, 140);
+            this.txtVOverlap.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtVOverlap.Location = new System.Drawing.Point(154, 206);
+            this.txtVOverlap.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtVOverlap.Name = "txtVOverlap";
-            this.txtVOverlap.Size = new System.Drawing.Size(200, 21);
+            this.txtVOverlap.Size = new System.Drawing.Size(298, 31);
             this.txtVOverlap.TabIndex = 7;
             // 
             // lblThreshold
             // 
             this.lblThreshold.AutoSize = true;
-            this.lblThreshold.Location = new System.Drawing.Point(20, 180);
+            this.lblThreshold.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblThreshold.Location = new System.Drawing.Point(30, 270);
+            this.lblThreshold.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblThreshold.Name = "lblThreshold";
-            this.lblThreshold.Size = new System.Drawing.Size(29, 12);
+            this.lblThreshold.Size = new System.Drawing.Size(50, 24);
             this.lblThreshold.TabIndex = 8;
             this.lblThreshold.Text = "阈值:";
             // 
             // txtThreshold
             // 
-            this.txtThreshold.Location = new System.Drawing.Point(150, 180);
+            this.txtThreshold.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtThreshold.Location = new System.Drawing.Point(154, 266);
+            this.txtThreshold.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtThreshold.Name = "txtThreshold";
-            this.txtThreshold.Size = new System.Drawing.Size(200, 21);
+            this.txtThreshold.Size = new System.Drawing.Size(298, 31);
             this.txtThreshold.TabIndex = 9;
             // 
             // lblIouThreshold
             // 
             this.lblIouThreshold.AutoSize = true;
-            this.lblIouThreshold.Location = new System.Drawing.Point(20, 220);
+            this.lblIouThreshold.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblIouThreshold.Location = new System.Drawing.Point(30, 330);
+            this.lblIouThreshold.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblIouThreshold.Name = "lblIouThreshold";
-            this.lblIouThreshold.Size = new System.Drawing.Size(53, 12);
+            this.lblIouThreshold.Size = new System.Drawing.Size(83, 24);
             this.lblIouThreshold.TabIndex = 10;
             this.lblIouThreshold.Text = "IOU阈值:";
             // 
             // txtIouThreshold
             // 
-            this.txtIouThreshold.Location = new System.Drawing.Point(150, 220);
+            this.txtIouThreshold.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtIouThreshold.Location = new System.Drawing.Point(154, 326);
+            this.txtIouThreshold.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtIouThreshold.Name = "txtIouThreshold";
-            this.txtIouThreshold.Size = new System.Drawing.Size(200, 21);
+            this.txtIouThreshold.Size = new System.Drawing.Size(298, 31);
             this.txtIouThreshold.TabIndex = 11;
             // 
             // lblCombineIosThreshold
             // 
             this.lblCombineIosThreshold.AutoSize = true;
-            this.lblCombineIosThreshold.Location = new System.Drawing.Point(20, 260);
+            this.lblCombineIosThreshold.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblCombineIosThreshold.Location = new System.Drawing.Point(30, 390);
+            this.lblCombineIosThreshold.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCombineIosThreshold.Name = "lblCombineIosThreshold";
-            this.lblCombineIosThreshold.Size = new System.Drawing.Size(77, 12);
+            this.lblCombineIosThreshold.Size = new System.Drawing.Size(116, 24);
             this.lblCombineIosThreshold.TabIndex = 12;
             this.lblCombineIosThreshold.Text = "合并IOS阈值:";
             // 
             // txtCombineIosThreshold
             // 
-            this.txtCombineIosThreshold.Location = new System.Drawing.Point(150, 260);
+            this.txtCombineIosThreshold.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtCombineIosThreshold.Location = new System.Drawing.Point(154, 386);
+            this.txtCombineIosThreshold.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCombineIosThreshold.Name = "txtCombineIosThreshold";
-            this.txtCombineIosThreshold.Size = new System.Drawing.Size(200, 21);
+            this.txtCombineIosThreshold.Size = new System.Drawing.Size(298, 31);
             this.txtCombineIosThreshold.TabIndex = 13;
             // 
             // btnOK
             // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOK.Location = new System.Drawing.Point(150, 300);
+            this.btnOK.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnOK.Location = new System.Drawing.Point(182, 445);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.Size = new System.Drawing.Size(120, 40);
             this.btnOK.TabIndex = 14;
             this.btnOK.Text = "确定";
             this.btnOK.UseVisualStyleBackColor = true;
@@ -171,10 +202,13 @@ namespace DlcvDemo
             // 
             // btnCancel
             // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(250, 300);
+            this.btnCancel.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCancel.Location = new System.Drawing.Point(332, 445);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(120, 40);
             this.btnCancel.TabIndex = 15;
             this.btnCancel.Text = "取消";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -182,10 +216,10 @@ namespace DlcvDemo
             // SlidingWindowConfigForm
             // 
             this.AcceptButton = this.btnOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.ClientSize = new System.Drawing.Size(485, 515);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.txtCombineIosThreshold);
@@ -203,6 +237,7 @@ namespace DlcvDemo
             this.Controls.Add(this.txtWidth);
             this.Controls.Add(this.lblWidth);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SlidingWindowConfigForm";
