ad873a72afb55e88b84bbadd375636a6a36d9dad
杨培文 (Yang Peiwen) <915505626@qq.com>
2025-06-01 20:48:20 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import shutil
 
 from setuptools import setup
 
-version = '2025.6.1.0'
+version = '2025.6.1.1'
 
 package_name = "dlcvpro_infer_csharp"  # 包名
 packages: list = [package_name]  # 需要打包的包
