0c82146d0d44fd5be225e9efc4f158b864629bc2
杨培文 (Yang Peiwen) <915505626@qq.com>
2025-06-07 23:19:56 +0800

    支持dvp推理 (#50)
    * Update Form1.cs
    
    * Update dlcv_infer_csharp.cs
    
    * Update dlcv_infer_csharp.cs
    
    * Update dlcv_infer_csharp.cs
    
    * Update setup.py
    
    * Update 1_编译打包.bat
    
    * Update setup.py

--- 1_编译打包.bat
+++ 1_编译打包.bat
@@ -6,7 +6,6 @@ mkdir dlcvpro_infer_csharp
 xcopy DlcvDemo\bin\*.exe dlcvpro_infer_csharp\ /Y
 xcopy DlcvDemo\bin\*.config dlcvpro_infer_csharp\ /Y
 xcopy DlcvDemo\bin\*.dll dlcvpro_infer_csharp\ /Y
-xcopy DlcvDemo\bin\dll\x64\OpenCvSharpExtern.dll dlcvpro_infer_csharp\ /Y
 
 C:\sign-tool\signtool.exe sign /n 深圳市深度视觉科技有限公司 /t http://time.certum.pl /fd sha256 /v  "dlcvpro_infer_csharp\C# 测试程序.exe"
 
