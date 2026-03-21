459b0ef1a7ed35d034b092d50348c9bd87f3e4df
杨培文 (Yang Peiwen) <915505626@qq.com>
2026-03-21 11:13:41 +0800

    Update main.cpp

--- dlcv_infer_cpp_dll_demo/main.cpp
+++ dlcv_infer_cpp_dll_demo/main.cpp
@@ -1,8 +1,10 @@
-#ifndef NOMINMAX
+﻿#ifndef NOMINMAX
 #define NOMINMAX
 #endif
 
+#include <fcntl.h>
 #include <iostream>
+#include <io.h>
 #include <string>
 
 #include <opencv2/imgcodecs.hpp>
@@ -11,15 +13,16 @@
 #include "dlcv_infer.h"
 
 int main() {
-    const std::string model_path = R"(Z:\A251113-微组半导体-IC封装检测\99-模型打包\微组-文字分类_120_50.dvst)";
-    const std::string img_path = R"(Z:\A251113-微组半导体-IC封装检测\99-模型打包\Fail1_965_574.tif)";
+
+    const std::wstring model_path = LR"(Z:\A251113-微组半导体-IC封装检测\99-模型打包\微组-文字分类_120_50.dvst)";
+    const std::wstring img_path = LR"(Z:\A251113-微组半导体-IC封装检测\99-模型打包\Fail1_965_574.tif)";
 
     try {
-        dlcv_infer::Model model(dlcv_infer::convertUtf8ToWstring(model_path), 0);
+        dlcv_infer::Model model(model_path, 0);
 
-        cv::Mat img = cv::imread(dlcv_infer::convertUtf8ToGbk(img_path), cv::IMREAD_COLOR);
+        cv::Mat img = cv::imread(dlcv_infer::convertWstringToGbk(img_path), cv::IMREAD_COLOR);
         if (img.empty()) {
-            std::cerr << "读取图片失败: " << img_path << std::endl;
+            std::wcerr << L"读取图片失败: " << img_path << std::endl;
             return 1;
         }
 
@@ -29,14 +32,14 @@ int main() {
         const dlcv_infer::Result result = model.Infer(img_rgb);
         for (const auto& sample_result : result.sampleResults) {
             for (const auto& object_result : sample_result.results) {
-                std::cout << "类别名称: " << object_result.categoryName
-                          << "，置信度: " << object_result.score
-                          << "，检测框: " << object_result.bbox[0] << ", " << object_result.bbox[1] << ", "
-                          << object_result.bbox[2] << ", " << object_result.bbox[3] << std::endl;
+                std::wcout << L"类别名称: " << dlcv_infer::convertUtf8ToWstring(object_result.categoryName)
+                           << L"，置信度: " << object_result.score
+                           << L"，检测框: " << object_result.bbox[0] << L", " << object_result.bbox[1] << L", "
+                           << object_result.bbox[2] << L", " << object_result.bbox[3] << std::endl;
             }
         }
     } catch (const std::exception& ex) {
-        std::cerr << "推理失败: " << ex.what() << std::endl;
+        std::wcerr << L"推理失败: " << dlcv_infer::convertUtf8ToWstring(ex.what()) << std::endl;
         return 1;
     }
 
