be6b89d5fe88c844cfbbddde5ecff61a21fc6629
杨培文 (Yang Peiwen) <915505626@qq.com>
2026-05-16 21:05:52 +0800

    chore: bump version to 2026.5.16.1a0

--- DlcvDemo/Properties/AssemblyInfo.cs
+++ DlcvDemo/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("2026.5.16.0")]
-[assembly: AssemblyFileVersion("2026.5.16.0")]
-[assembly: AssemblyInformationalVersion("2026.5.16.0a0")]
+[assembly: AssemblyVersion("2026.5.16.1")]
+[assembly: AssemblyFileVersion("2026.5.16.1")]
+[assembly: AssemblyInformationalVersion("2026.5.16.1a0")]
 [assembly: NeutralResourcesLanguage("zh-CN")]
