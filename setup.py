e3bda4e7f2aff5959a9fbee2e6e8b7b9140fa983
杨培文 (Yang Peiwen) <915505626@qq.com>
2026-05-25 14:53:15 +0800

    chore(release): bump version to 2026.5.25.0 (#177)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import shutil
 
 from setuptools import setup
 
-version = '2026.5.16.1a0'
+version = '2026.5.25.0'
 
 package_name = "dlcvpro_infer_csharp"  # 包名
 packages: list = [package_name]  # 需要打包的包
