cddc477baf8c4380027b8dcd1c863fd5b7c06ee3
杨培文 (Yang Peiwen) <915505626@qq.com>
2026-08-19 18:02:31 +0800

    feat: 完善结果文本编辑菜单

--- DlcvDemo/Properties/AssemblyInfo.cs
+++ DlcvDemo/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("2026.8.19.1")]
-[assembly: AssemblyFileVersion("2026.8.19.1")]
-[assembly: AssemblyInformationalVersion("2026.8.19.1a0")]
+[assembly: AssemblyVersion("2026.8.19.2")]
+[assembly: AssemblyFileVersion("2026.8.19.2")]
+[assembly: AssemblyInformationalVersion("2026.8.19.2a0")]
 [assembly: NeutralResourcesLanguage("zh-CN")]
