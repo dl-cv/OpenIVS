57bc7e25388c93b885dc511c4ed289bc18131726
hamletzhang <38814322+hamletzhang@users.noreply.github.com>
2026-08-20 21:44:47 +0800

    C# 测试程序打印流程输出状态和原因 (#215)
    * 在 C# 测试结果中打印流程状态
    
    * 将 C# 版本更新到 2026.8.20.2

--- DlcvDemo/Properties/AssemblyInfo.cs
+++ DlcvDemo/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("2026.8.20.1")]
-[assembly: AssemblyFileVersion("2026.8.20.1")]
-[assembly: AssemblyInformationalVersion("2026.8.20.1")]
+[assembly: AssemblyVersion("2026.8.20.2")]
+[assembly: AssemblyFileVersion("2026.8.20.2")]
+[assembly: AssemblyInformationalVersion("2026.8.20.2")]
 [assembly: NeutralResourcesLanguage("zh-CN")]
