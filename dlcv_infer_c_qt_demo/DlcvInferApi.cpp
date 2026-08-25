d0497ad0efcc3d867c13eb72c67950bb7c3360d2
杨培文 (Yang Peiwen) <915505626@qq.com>
2026-08-26 04:06:54 +0800

    feat: 扩展 Qt Demo 全量 C 接口加载

--- dlcv_infer_c_qt_demo/DlcvInferApi.cpp
+++ dlcv_infer_c_qt_demo/DlcvInferApi.cpp
@@ -18,6 +18,7 @@ bool DlcvInferApi::load() {
     if (!resolve("dlcv_infer_cpp_load_model_c", loadModel_) ||
         !resolve("dlcv_infer_cpp_get_last_error_c", getLastError_) ||
         !resolve("dlcv_infer_cpp_free_model_c", freeModel_) ||
+        !resolve("dlcv_infer_cpp_infer_c", infer_) ||
         !resolve("dlcv_infer_cpp_infer_with_params_c", inferWithParams_) ||
         !resolve("dlcv_infer_cpp_free_model_result_c", freeModelResult_) ||
         !resolve("dlcv_infer_cpp_get_model_info_c", getModelInfo_) ||
@@ -25,9 +26,29 @@ bool DlcvInferApi::load() {
         !resolve("dlcv_infer_cpp_get_all_dog_info_c", getAllDogInfo_) ||
         !resolve("dlcv_infer_cpp_free_string_c", freeString_) ||
         !resolve("dlcv_infer_cpp_free_all_models_c", freeAllModels_) ||
-        !resolve("dlcv_keep_max_clock", keepMaxClock_) ||
-        !resolve("dlcv_get_gpu_info", getGpuInfo_) ||
-        !resolve("dlcv_free_result", freeSystemString_)) {
+        !resolve("dlcv_load_model_c", loadModelCompatible_) ||
+        !resolve("dlcv_free_model_c", freeModelCompatible_) ||
+        !resolve("dlcv_infer_c", inferCompatible_) ||
+        !resolve("dlcv_free_model_result_c", freeModelResultCompatible_) ||
+        !resolve("dlcv_load_model", loadModelNative_) ||
+        !resolve("dlcv_free_model", freeModelNative_) ||
+        !resolve("dlcv_get_model_info", getModelInfoNative_) ||
+        !resolve("dlcv_infer", inferNative_) ||
+        !resolve("dlcv_free_model_result", freeModelResultNative_) ||
+        !resolve("dlcv_free_result", freeResultNative_) ||
+        !resolve("dlcv_free_all_models", freeAllModelsNative_) ||
+        !resolve("dlcv_get_device_info", getDeviceInfoNative_) ||
+        !resolve("dlcv_get_gpu_info", getGpuInfoNative_) ||
+        !resolve("dlcv_keep_max_clock", keepMaxClockNative_) ||
+        !resolve("dlcv_reset_max_clock", resetMaxClockNative_) ||
+        !resolve("dlcv_set_gpu_max_clock", setGpuMaxClockNative_) ||
+        !resolve("dlcv_reset_gpu_max_clock", resetGpuMaxClockNative_) ||
+        !resolve("dlcv_get_power_scheme_guid", getPowerSchemeGuidNative_) ||
+        !resolve("dlcv_set_power_scheme_guid", setPowerSchemeGuidNative_) ||
+        !resolve("dlcv_get_power_scheme", getPowerSchemeNative_) ||
+        !resolve("dlcv_set_power_scheme", setPowerSchemeNative_) ||
+        !resolve("dlcv_set_current_process_affinity_to_big_cores", setCurrentProcessAffinityToBigCoresNative_) ||
+        !resolve("dlcv_set_current_process_priority_highest", setCurrentProcessPriorityHighestNative_)) {
         FreeLibrary(module_);
         module_ = nullptr;
         clearFunctions();
@@ -66,6 +87,10 @@ int DlcvInferApi::freeModel(int modelIndex) const {
     return freeModel_(modelIndex);
 }
 
+DlcvCResult DlcvInferApi::infer(int modelIndex, const DlcvCImageList* imageList) const {
+    return infer_(modelIndex, imageList);
+}
+
 DlcvCResult DlcvInferApi::inferWithParams(
     int modelIndex,
     const DlcvCImageList* imageList,
@@ -100,23 +125,119 @@ void DlcvInferApi::freeAllModels() const {
 }
 
 void DlcvInferApi::keepMaxClock() const {
-    keepMaxClock_();
+    keepMaxClockNative();
 }
 
 const char* DlcvInferApi::getGpuInfo() const {
-    return getGpuInfo_();
+    return getGpuInfoNative();
 }
 
 void DlcvInferApi::freeSystemString(const char* value) const {
-    if (value != nullptr) {
-        freeSystemString_(value);
+    freeResultNative(value);
+}
+
+int DlcvInferApi::loadModelCompatible(const char* modelPath, int deviceId) const {
+    return loadModelCompatible_(modelPath, deviceId);
+}
+
+int DlcvInferApi::freeModelCompatible(int modelIndex) const {
+    return freeModelCompatible_(modelIndex);
+}
+
+DlcvCResult DlcvInferApi::inferCompatible(int modelIndex, const DlcvCImageList* imageList) const {
+    return inferCompatible_(modelIndex, imageList);
+}
+
+void DlcvInferApi::freeModelResultCompatible(DlcvCResult* result) const {
+    freeModelResultCompatible_(result);
+}
+
+const char* DlcvInferApi::loadModelNative(const char* configStr) const {
+    return loadModelNative_(configStr);
+}
+
+const char* DlcvInferApi::freeModelNative(const char* configStr) const {
+    return freeModelNative_(configStr);
+}
+
+const char* DlcvInferApi::getModelInfoNative(const char* configStr) const {
+    return getModelInfoNative_(configStr);
+}
+
+const char* DlcvInferApi::inferNative(const char* configStr) const {
+    return inferNative_(configStr);
+}
+
+void DlcvInferApi::freeModelResultNative(const char* configStr) const {
+    freeModelResultNative_(configStr);
+}
+
+void DlcvInferApi::freeResultNative(const char* configStr) const {
+    if (configStr != nullptr) {
+        freeResultNative_(configStr);
     }
+}
+
+void DlcvInferApi::freeAllModelsNative() const {
+    freeAllModelsNative_();
+}
+
+const char* DlcvInferApi::getDeviceInfoNative() const {
+    return getDeviceInfoNative_();
+}
+
+const char* DlcvInferApi::getGpuInfoNative() const {
+    return getGpuInfoNative_();
+}
+
+void DlcvInferApi::keepMaxClockNative() const {
+    keepMaxClockNative_();
+}
+
+void DlcvInferApi::resetMaxClockNative() const {
+    resetMaxClockNative_();
+}
+
+void DlcvInferApi::setGpuMaxClockNative(bool verbose) const {
+    setGpuMaxClockNative_(verbose);
+}
+
+void DlcvInferApi::resetGpuMaxClockNative(bool verbose) const {
+    resetGpuMaxClockNative_(verbose);
+}
+
+const char* DlcvInferApi::getPowerSchemeGuidNative(int verbose) const {
+    return getPowerSchemeGuidNative_(verbose);
+}
+
+int DlcvInferApi::setPowerSchemeGuidNative(const char* schemeGuid, int verbose) const {
+    return setPowerSchemeGuidNative_(schemeGuid, verbose);
+}
+
+const char* DlcvInferApi::getPowerSchemeNative(int verbose) const {
+    return getPowerSchemeNative_(verbose);
+}
+
+int DlcvInferApi::setPowerSchemeNative(const char* schemeName, int verbose) const {
+    return setPowerSchemeNative_(schemeName, verbose);
+}
+
+int DlcvInferApi::setCurrentProcessAffinityToBigCoresNative(int verbose) const {
+    return setCurrentProcessAffinityToBigCoresNative_(verbose);
+}
+
+int DlcvInferApi::setCurrentProcessPriorityHighestNative(
+    int preferRealtime,
+    int verbose,
+    int bindBigCores) const {
+    return setCurrentProcessPriorityHighestNative_(preferRealtime, verbose, bindBigCores);
 }
 
 void DlcvInferApi::clearFunctions() {
     loadModel_ = nullptr;
     getLastError_ = nullptr;
     freeModel_ = nullptr;
+    infer_ = nullptr;
     inferWithParams_ = nullptr;
     freeModelResult_ = nullptr;
     getModelInfo_ = nullptr;
@@ -124,9 +245,29 @@ void DlcvInferApi::clearFunctions() {
     getAllDogInfo_ = nullptr;
     freeString_ = nullptr;
     freeAllModels_ = nullptr;
-    keepMaxClock_ = nullptr;
-    getGpuInfo_ = nullptr;
-    freeSystemString_ = nullptr;
+    loadModelCompatible_ = nullptr;
+    freeModelCompatible_ = nullptr;
+    inferCompatible_ = nullptr;
+    freeModelResultCompatible_ = nullptr;
+    loadModelNative_ = nullptr;
+    freeModelNative_ = nullptr;
+    getModelInfoNative_ = nullptr;
+    inferNative_ = nullptr;
+    freeModelResultNative_ = nullptr;
+    freeResultNative_ = nullptr;
+    freeAllModelsNative_ = nullptr;
+    getDeviceInfoNative_ = nullptr;
+    getGpuInfoNative_ = nullptr;
+    keepMaxClockNative_ = nullptr;
+    resetMaxClockNative_ = nullptr;
+    setGpuMaxClockNative_ = nullptr;
+    resetGpuMaxClockNative_ = nullptr;
+    getPowerSchemeGuidNative_ = nullptr;
+    setPowerSchemeGuidNative_ = nullptr;
+    getPowerSchemeNative_ = nullptr;
+    setPowerSchemeNative_ = nullptr;
+    setCurrentProcessAffinityToBigCoresNative_ = nullptr;
+    setCurrentProcessPriorityHighestNative_ = nullptr;
 }
 
 std::wstring DlcvInferApi::formatSystemError(DWORD errorCode) {
--- dlcv_infer_c_qt_demo/main.cpp
+++ dlcv_infer_c_qt_demo/main.cpp
@@ -1,9 +1,23 @@
 #include <QApplication>
 #include <QFont>
 
+#include <cstdio>
+#include <cstring>
+
+#include "DlcvInferApi.h"
 #include "MainWindow.h"
 
 int main(int argc, char* argv[]) {
+    if (argc == 2 && std::strcmp(argv[1], "--check-c-api-exports") == 0) {
+        DlcvInferApi api;
+        if (api.load()) {
+            std::puts("C API 导出检查通过");
+            return 0;
+        }
+        std::fwprintf(stderr, L"C API 导出检查失败：%ls\n", api.lastError().c_str());
+        return 1;
+    }
+
     QApplication app(argc, argv);
     app.setApplicationName("C测试程序");
     app.setOrganizationName("dlcv");
