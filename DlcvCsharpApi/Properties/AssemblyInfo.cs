a32431ab3c1ce469b399999ed25797eb3b577b29
杨培文 (Yang Peiwen) <915505626@qq.com>
2026-09-02 13:04:21 +0800

    fix: 修复正式打包证书选择并发布2026.9.2.0

--- DlcvCsharpApi/Properties/AssemblyInfo.cs
+++ DlcvCsharpApi/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("2026.9.1.0")]
-[assembly: AssemblyFileVersion("2026.9.1.0")]
-[assembly: AssemblyInformationalVersion("2026.9.1.0a0")]
+[assembly: AssemblyVersion("2026.9.2.0")]
+[assembly: AssemblyFileVersion("2026.9.2.0")]
+[assembly: AssemblyInformationalVersion("2026.9.2.0")]
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import shutil
 
 from setuptools import setup
 
-version = '2026.9.1.0a0'
+version = '2026.9.2.0'
 
 package_name = "dlcvpro_infer_csharp"  # 包名
 packages: list = [package_name]  # 需要打包的包
