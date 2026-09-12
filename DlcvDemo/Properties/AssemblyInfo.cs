904d622ef14a3788c7740966c6b38ac497dbb210
杨培文 (Yang Peiwen) <915505626@qq.com>
2026-09-13 01:21:06 +0800

    chore: 更新合并日期测试版

--- DlcvDemo/Properties/AssemblyInfo.cs
+++ DlcvDemo/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("2026.9.12.0")]
-[assembly: AssemblyFileVersion("2026.9.12.0")]
-[assembly: AssemblyInformationalVersion("2026.9.12.0a0")]
+[assembly: AssemblyVersion("2026.9.13.0")]
+[assembly: AssemblyFileVersion("2026.9.13.0")]
+[assembly: AssemblyInformationalVersion("2026.9.13.0a0")]
 [assembly: NeutralResourcesLanguage("zh-CN")]
